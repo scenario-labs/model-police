0c5e8d48fef923854d01d5c9ea7c951f25d6fe59
christopher5106 <christopher.bourez@gmail.com>
2025-09-02 11:38:11 +0000

    feat(inference-ml): wip

--- model_police/dump_layer_names_with_shapes_from_checkpoint.py
+++ model_police/dump_layer_names_with_shapes_from_checkpoint.py
@@ -8,14 +8,16 @@ from pathlib import Path
 
 from model_police import ModelPolice
 
+here = Path(__file__).parent
+
 def main():
 
     model_police_officer = ModelPolice()
 
     parser = argparse.ArgumentParser()
     parser.add_argument("checkpoint")
-    parser.add_argument("model", help="Flux, SDXL, SD15...", default="")
-    parser.add_argument("framework", help="Diffusers, Kohya, Unknown", default="")
+    parser.add_argument("model", help="Flux, SDXL, SD15...", default="", nargs='?')
+    parser.add_argument("framework", help="Diffusers, Kohya, Unknown", default="", nargs='?')
     args = parser.parse_args()
 
     is_lora, model_classes, layer_names_with_shapes, error = model_police_officer.inspect(args.checkpoint)
@@ -24,7 +26,14 @@ def main():
         raise Exception(error)
 
     if args.model and args.framework:
+        full_model_dict = here / "model_dictionaries" / f"{args.model}_{args.framework}.csv".lower()
+        if full_model_dict.exists() and input(f"File {full_model_dict} already exists, override it ? [y/N]") != "y":
+            exit()
+        with open(full_model_dict, "w") as f:
+            for k in layer_names_with_shapes:
+                f.write(k + "\n")
 
+    else:
         for k in layer_names_with_shapes:
             print(k)
 
